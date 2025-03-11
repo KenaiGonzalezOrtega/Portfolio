7ac10e55d849c9e355f6556bac740ed6181d30b0
KenaiGonzalezOrtega <113781990+KenaiGonzalezOrtega@users.noreply.github.com>
2025-03-11 11:43:13 +0000

    Add new translations

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -15,14 +15,67 @@ i18n
     resources: {
       en: {
         translation: {
+          header: {
+            presentation: "Hi, I'm",
+            description: {
+              part1: "Computer Engineer passionate about technology and innovation.",
+              part2: "I am constantly looking for new challenges to create innovative solutions."
+            }
+          },
+
+          about: {
+            title: "About me",
+            description: "I'm a full-stack developer with a focus on creating solutions that solve real problems. My passion for technology leads me to constantly explore new tools and methodologies. I seek to contribute to innovative projects and continue learning along the way."
+          },
+          skills: {
+            title: "My skills"
+          },
+          softSkills: {
+            title: "Soft skills",
+            texts: {
+              "Team work": "Team work",
+              "Adaptable": "Adaptability",
+              "Problem solving": "Problem solving",
+              "Attention to detail": "Attention to detail",
+              "Open Mindedness": "Open Mindedness",
+              "Continuous Learning": "Continuous Learning"
+            }
+          }
 
         }
       },
 
       es: {
         translation: {
+          header: {
+            presentation: "Hola, soy",
+            description: {
+              part1: "Ingeniero Informático apasionado por la tecnología y la innovación.",
+              part2: "Estoy en constante búsqueda de nuevos desafíos para crear soluciones innovadoras."
+            }
+
+          },
+          about: {
+            title: "Sobre mí",
+            description: "Soy un desarrollador full-stack con un enfoque en la creación de soluciones que resuelvan problemas reales. Mi pasión por la tecnología me lleva a explorar constantemente nuevas herramientas y metodologías. Busco contribuir a proyectos innovadores y seguir aprendiendo a lo largo del camino."
+          },
+          skills: {
+            title: "Mis habilidades"
+          },
+          softSkills: {
+            title: "Habilidades Personales",
+            texts: {
+              "Team work": "Trabajo en equipo",
+              "Adaptable": "Adaptabilidad",
+              "Problem solving": "Resolución de problemas",
+              "Attention to detail": "Atención al detalle",
+              "Open Mindedness": "Mentalidad abierta",
+              "Continuous Learning": "Aprendizaje continuo"
+            }
+          }
 
         }
+
       }
     }
   });
