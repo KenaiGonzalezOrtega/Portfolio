dda7eef1967888b732b33540bc1cb4de7a0eabac
KenaiGonzalezOrtega <113781990+KenaiGonzalezOrtega@users.noreply.github.com>
2025-03-11 11:41:04 +0000

    Add Carousel component and arrow icons

---
+++ src/components/Carousel.tsx
@@ -0,0 +1,34 @@
+import { useState } from "react"
+import LeftArrowIcon from "../icons/LeftArrowIcon"
+import RightArrowIcon from "../icons/RightArrowIcon"
+
+export default function Carousel({ visibleItems, totalItems, children }: { visibleItems: number, totalItems: number, children: React.ReactNode }) {
+
+  const containerWidth = Math.ceil(totalItems / visibleItems) * 100
+  const totalPages = Math.ceil(totalItems / visibleItems)
+  const pageWidth = containerWidth / totalPages
+  const [page, setPage] = useState(0)
+  const handleClick = (num: number) => {
+    setPage(prev => prev + num)
+  }
+
+  return (
+    <div className="relative w-full overflow-hidden">
+      <button disabled={(page <= 0)} type="button" className="cursor-pointer absolute left-4 top-1/2 transform -translate-y-1/2 p-2 bg-white rounded-full shadow hover:bg-gray-200 z-10 disabled:opacity-40 hover:disabled:bg-white disabled:cursor-auto" onClick={handleClick.bind(null, -1)}>
+        <LeftArrowIcon />
+      </button>
+      <div className={"grid grid-cols-1 transition-transform duration-500 "}
+        style={{
+          width: `${containerWidth}%`,
+          transform: `translateX(-${(pageWidth * page) / totalPages}%)`,
+          gridTemplateColumns: `repeat(${visibleItems * totalPages}, minmax(0, 1fr))`
+        }}
+      >
+        {children}
+      </div>
+      <button disabled={(page >= totalPages - 1)} type="button" className="cursor-pointer absolute right-4 top-1/2 transform -translate-y-1/2 p-2 bg-white rounded-full shadow hover:bg-gray-200 z-10 disabled:opacity-40 hover:disabled:bg-white disabled:cursor-auto" onClick={handleClick.bind(null, 1)}>
+        <RightArrowIcon />
+      </button>
+    </div>
+  )
+}
---
+++ src/icons/RightArrowIcon.tsx
@@ -0,0 +1,9 @@
+export default function RightArrowIcon() {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
+      <title>Right Arrow Icon</title>
+      <path strokeLinecap="round" strokeLinejoin="round" d="m5.25 4.5 7.5 7.5-7.5 7.5m6-15 7.5 7.5-7.5 7.5" />
+    </svg>
+
+  )
+}
