a83f54db0d6d7a72e591718eafbf1745856125bc
KenaiGonzalezOrtega <113781990+KenaiGonzalezOrtega@users.noreply.github.com>
2025-03-20 11:05:38 +0000

    Adjusting the Position of the Navigation Buttons in the Carousel Component

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -14,7 +14,7 @@ export default function Carousel({ visibleItems, totalItems, children }: { visib
 
   return (
     <div className="relative w-full overflow-hidden text-black">
-      <button disabled={(page <= 0)} type="button" className="cursor-pointer absolute left-4 top-1/2 transform -translate-y-1/2 p-2 bg-white rounded-full shadow hover:bg-gray-200 z-10 disabled:opacity-40 hover:disabled:bg-white disabled:cursor-auto" onClick={handleClick.bind(null, -1)}>
+      <button disabled={(page <= 0)} type="button" className="cursor-pointer absolute left-2 md:left-4 top-1/2 transform -translate-y-1/2 p-2 bg-white rounded-full shadow hover:bg-gray-200 z-10 disabled:opacity-40 hover:disabled:bg-white disabled:cursor-auto" onClick={handleClick.bind(null, -1)}>
         <LeftArrowIcon />
       </button>
       <div className={"grid grid-cols-1 transition-transform duration-500 justify-items-center "}
@@ -26,7 +26,7 @@ export default function Carousel({ visibleItems, totalItems, children }: { visib
       >
         {children}
       </div>
-      <button disabled={(page >= totalPages - 1)} type="button" className="cursor-pointer absolute right-4 top-1/2 transform -translate-y-1/2 p-2 bg-white rounded-full shadow hover:bg-gray-200 z-10 disabled:opacity-40 hover:disabled:bg-white disabled:cursor-auto" onClick={handleClick.bind(null, 1)}>
+      <button disabled={(page >= totalPages - 1)} type="button" className="cursor-pointer absolute right-2 md:right-4 top-1/2 transform -translate-y-1/2 p-2 bg-white rounded-full shadow hover:bg-gray-200 z-10 disabled:opacity-40 hover:disabled:bg-white disabled:cursor-auto" onClick={handleClick.bind(null, 1)}>
         <RightArrowIcon />
       </button>
     </div>
